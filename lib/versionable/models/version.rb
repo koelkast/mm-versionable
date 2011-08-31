a25e76631753ae138182a8c238ad12f1914de998
Jean Jacques Warmerdam <sjaq@koelka.st>
2011-08-31 11:57:57 +0200

    fixes

--- lib/versionable/models/version.rb
+++ lib/versionable/models/version.rb
@@ -4,10 +4,12 @@ class Version
   key :data, Hash
   key :date, Time
   key :pos, Integer, :index => true
-  key :doc_id, String, :index => true
-  key :message, String
-  key :updater_id, String
+  key :doc_id, ObjectId, :index => true
+  
+  key :updater_id, ObjectId
+
   key :type, String
+  key :event, String
 
   def content(key)
     cdata = self.data[key]
@@ -16,5 +18,23 @@ class Version
     else
       cdata
     end
-  end  
+  end 
+  
+  def user
+    return User.find self.updater_id
+  end
+  
+  def get
+    unless defined? @klass
+      @klass = self.type.constantize.new
+      @klass.attributes = self.data
+    end
+    
+    return @klass
+  end
+  
+  def exists?
+    !self.type.constantize.find(self.doc_id).nil?
+  end
+  
 end
--- lib/versionable/plugins/versionable.rb
+++ lib/versionable/plugins/versionable.rb
@@ -5,29 +5,29 @@ module Versionable
 
   module InstanceMethods
     # Save new versions but only if the data changes
-    def save(options={})
-      save_version(options.delete(:updater_id)) if self.respond_to?(:rolling_back) && !rolling_back
-      super
-    end
+    # def save(options={})
+    #   save_version(options.delete(:updater_id)) if self.respond_to?(:rolling_back) && !rolling_back
+    #   super
+    # end
 
-    def update_attributes(attrs={}, options={})
-      save_version(options.delete(:updater_id)) if self.respond_to?(:rolling_back) && !rolling_back
-      super attrs
-    end
+    # def update_attributes(attrs={}, options={})
+    #   # save_version(options.delete(:updater_id)) if self.respond_to?(:rolling_back) && !rolling_back
+    #   super attrs
+    # end
 
-    def save_version(updater_id=nil)
+    def save_version(event = :create)
       if self.respond_to?(:versions)
         version = self.current_version
-        version.message = self.version_message
+        version.event = event.to_s.downcase
         if self.versions.empty?
           version.pos = 0
         else
           version.pos = self.versions.last.pos + 1
         end
         if self.version_at(self.version_number).try(:data) != version.data
-          version.updater_id = updater_id
+          puts self.class.to_s + " " + event.to_s
           version.type = self.class.to_s
-          if defined? User and User.respond_to?(:current) and updater_id == nil
+          if defined? User and User.respond_to?(:current)
             version.updater_id = User.current.id
           end
           version.save
@@ -44,16 +44,25 @@ module Versionable
 
   module ClassMethods
     def enable_versioning(opts={})
+      
+      after_create :record_create
+      before_update :record_update
+      after_destroy :record_destroy
+
       attr_accessor :rolling_back
 
       key :version_number, Integer, :default => 0
-
-      define_method(:version_message) do
-        @version_message
+      
+      define_method(:record_create) do
+        save_version(:create) if self.respond_to?(:rolling_back) && !rolling_back
       end
-
-      define_method(:version_message=) do |message|
-        @version_message = message
+      
+      define_method(:record_update) do
+        save_version(:update) if self.respond_to?(:rolling_back) && !rolling_back
+      end
+      
+      define_method(:record_destroy) do
+        save_version(:destroy) if self.respond_to?(:rolling_back) && !rolling_back
       end
 
       define_method(:versions_count) do
@@ -104,7 +113,7 @@ module Versionable
 
       define_method(:current_version) do
         data = self.attributes
-        data.delete(:version_number)
+        data.except(:version_number, :updated_at, :created_at, :something_strange)
         Version.new(:data => data, :date => Time.now, :doc_id => self._id.to_s)
       end
 
